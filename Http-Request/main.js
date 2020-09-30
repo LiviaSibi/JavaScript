1f375763147c2e006bf6f872e1ab30c338d38af0
LiviaSibi <livia.sibinelli@gmail.com>
2020-09-30 11:16:25 -0300

    http request

---
+++ Http-Request/main.js
@@ -0,0 +1,88 @@
+const divElement = document.getElementById('container')
+const inputElement = document.querySelector("#container input")
+const searchButtonElement = document.getElementById('searchButton')
+const ulElement = document.querySelector("#container ul")
+
+
+//------------------------------------- USANDO AXIOS -------------------------------------
+inputElement.onkeypress = function(e) {
+  if (e.keyCode == 13) {
+    searchUser();
+    e.preventDefault();
+    inputElement.value = "";
+  };
+};
+
+searchButtonElement.onclick = function() {
+  searchUser();
+  inputElement.value = "";
+};
+
+function searchUser(user) {
+  document.getElementById("errorMessage").innerHTML="";
+  document.getElementById("lblLoading").innerHTML="Carregando....";
+  user = inputElement.value;
+  const url = 'https://api.github.com/users/'+ user + '/repos';
+  
+  axios
+  .get(url)
+  .then(function(response) {
+    document.getElementById("lblLoading").innerHTML="";
+    const repos = response.data;
+    for(let repo of repos) {
+      let liElement = document.createElement('li');
+      let textItem = document.createTextNode(repo.name);
+      liElement.appendChild(textItem);
+      divElement.appendChild(liElement);
+    };
+  })
+  .catch(function(error) {
+    document.getElementById("lblLoading").innerHTML="";
+    if(error.response.status == 404){
+      document.getElementById("errorMessage").innerHTML="Usuário (" + user + ") não existe."
+    }
+  })
+};
+
+
+//------------------------------------- USANDO HTTP REQUEST -------------------------------------
+var minhaPromise = function() {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    let user = inputElement.value;
+    const url = 'https://api.github.com/users/'+ user + '/repos';
+    xhr.open('GET', url);
+    xhr.send(null);
+
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState === 4) {
+        if(xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject('Erro', xhr.status, 'na requisição')
+        }
+      }
+    }
+  });
+}
+
+document.getElementById('searchButton2').onclick = function() {
+     minhaPromise()
+     .then(function(response){
+          document.getElementById("lblLoading").innerHTML="";
+          const repos = response;
+          for(let repo of repos) {
+          let liElement = document.createElement('li');
+          let textItem = document.createTextNode(repo.name);
+          liElement.appendChild(textItem);
+          divElement.appendChild(liElement);
+          };
+     })
+     .catch(function(error){
+          document.getElementById("lblLoading").innerHTML="";
+          let user = inputElement.value;
+          if(error.status == 404){
+               document.getElementById("errorMessage").innerHTML="Usuário (" + user + ") não existe."
+          }
+     });
+}
